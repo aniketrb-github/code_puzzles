aa7fc440c4bbc0a7a0d989d05d4260d2376fdc63
aniketrb-github <aniketbharsakale@gmail.com>
2019-05-08 00:23:31 +0530

    updated link list solution

--- code_puzzles/src/data_structures_algorithms/LinkedList.java
+++ code_puzzles/src/data_structures_algorithms/LinkedList.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class LinkedList {
 	
-	// head of list
+	// Reference to the Static Inner Class represents as the Head pointer of Linked List
 	Node head;
 	
-	// static class so that main() can access it
+	// The Node class is made static class so that main() can access it.
 	static class Node {
 		int data;
 		Node next;
@@ -19,19 +19,30 @@ public class LinkedList {
 		}
 	}
 	
+	// Driver Method
 	public static void main(String[] args) {
 		LinkedList linkList = new LinkedList();
 		linkList = createLinkedList(linkList);
+		int inputData = 0;
+		int afterNodeData = 0;
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Please enter a Integer number to create & append a Node: ");
-		int inputData = in.nextInt();
-		
-		appendNodeToList(linkList, inputData);
-		
-		System.out.println("Please enter a Integer number to create & prefix a Node: ");
+/*
+		System.out.println("Please enter an Integer number to create & append a Node: ");
 		inputData = in.nextInt();
-		prefixNodeToList(linkList, inputData);
+
+		appendNodeToList(linkList, inputData);
+
+		System.out.println("Please enter an Integer number to create & prefix a Node: ");
+		inputData = in.nextInt();
+		prefixNodeToList(linkList, inputData);*/
+
+		System.out.println("Please enter an Integer number to insert it in between the linked list: ");
+		inputData = in.nextInt();
+		System.out.println("After which node do u want to insert the new node? Please enter a Integer number: ");
+		afterNodeData = in.nextInt();
+		// insertNodeAfter(linkList, inputData, afterNodeData);
+		insertNodeBefore(linkList, inputData, afterNodeData);
 		
 		in.close();
 	}
@@ -77,6 +88,7 @@ public class LinkedList {
 			
 			System.out.print("LinkedList: ");
 			
+			// traverse through the linked list i.e head reaches the null value
 			while(currentNode != null) {
 				System.out.print(currentNode.data +" ");
 				currentNode = currentNode.next; // increment to next node
@@ -93,23 +105,23 @@ public class LinkedList {
 	 * @param data the node to be created & inserted 
 	 * @return list with the new node appended to it
 	 */
-	public static LinkedList appendNodeToList(LinkedList list, int data) {
+	public static void appendNodeToList(LinkedList list, int data) {
 		Node newNode = createNewNode(data);
-		
+		Node lastNode = null;
 		// if given list is empty, then make the incoming node as 1st node
 		if(list.head == null) {
 			list.head = newNode;
 		} else {
-			Node lastNode = list.head;
-			// traverse to the end of the list to get hold of the last node
-			while (lastNode.next != null) {
+			lastNode = list.head; // assume head as the only node in list or there can be many more 
+			
+			// traverse to the LAST NODE of the list & capture the last node
+			while(lastNode.next != null) {
 				lastNode = lastNode.next; // increment to next node
 			}
 			// once last node is found, we make the newNode as the lastNode
 			lastNode.next = newNode;
 		}
 		printLinkedList(list);
-		return list;
 	}
 	
 	/**
@@ -118,7 +130,7 @@ public class LinkedList {
 	 * @param data
 	 * @return list with the new node prefixed to it
 	 */
-	public static LinkedList prefixNodeToList(LinkedList list, int data) {
+	public static void prefixNodeToList(LinkedList list, int data) {
 		Node newNode = createNewNode(data);
 		
 		// if List is empty, the created node becomes the 1st node & the only node in the list
@@ -126,40 +138,37 @@ public class LinkedList {
 			list.head = newNode;
 		} else {
 		// if list is not empty, prefix the created node to be the 1st node of existing list
-			
 			Node oldFirstNode = list.head; 	// get hold of the existing 1st node
 			list.head = newNode;			// make newNode as 1st node in list
 			list.head.next = oldFirstNode;	// append oldFirstNode to latest/newest 1st node
 		}
 		printLinkedList(list);
-		return list;
 	}
 	
 	/**
-	 * 
+	 * Creates a Node of given data & inserts this node after the mentioned node in the given list
 	 * @param list
 	 * @param data
 	 * @return
 	 */
-	public static LinkedList insertNodeAfter(LinkedList list, int data, int afterNodeData) {
-		Node newNode = createNewNode(data);
-		Node currentNode = list.head;
-		boolean isNodePresent = false;
-		// check if afterNodeData exist in the given list
-		while(currentNode.next != null) {
-			if(currentNode.data == afterNodeData) {
-				isNodePresent = true;
-				break;
-			}
-			currentNode = currentNode.next; // increment to next node
-		}
+	public static void insertNodeAfter(LinkedList list, int data, int afterNodeData) {
+		Node newNode = createNewNode(data);	// create new node to be inserted using given data
+		Node currentNode = list.head;		// get hold of 1st node in list
+
+		// check if the given node exist in given linked List 
+		boolean isNodePresent = checkIfNodeExistInList(list, afterNodeData);
 		
-		Node tempNode = null;
+		Node nextNode = null;
 		if(isNodePresent) {
-			currentNode = list.head;
-			while(currentNode.next != null) {
+			currentNode = list.head;	// get the 1st node
+			
+			// traverse through the linked list i.e head reaches the null value
+			while(currentNode != null) {
 				if(currentNode.data == afterNodeData) {
-					tempNode = currentNode;
+					nextNode = currentNode.next;// get hold of nextNode 
+					currentNode.next = newNode;	// make the currentNode point to the newNode
+					newNode.next = nextNode;	// make the newNode point to the saved nextNode
+					break;
 				}
 				currentNode = currentNode.next;
 			}
@@ -168,31 +177,56 @@ public class LinkedList {
 			System.out.println("Given Node does not exist in the Linked List!");
 		}
 		
-		return list;
+		printLinkedList(list);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	/**
 	 * 
 	 * @param list
 	 * @param data
+	 * @return 
+	 */
+	public static void insertNodeBefore(LinkedList list, int data, int beforeNodeData) {
+		Node newNode = createNewNode(data);
+		Node currentNode = list.head;
+		Node previousNode = null;
+		
+		// check if the given node exist in given linked List 
+		boolean isNodePresent = checkIfNodeExistInList(list, beforeNodeData);
+		
+		if(isNodePresent) {
+			
+			while(currentNode != null) {
+				
+				if(currentNode.data == beforeNodeData) {
+					previousNode.next = newNode;
+					newNode.next = currentNode;
+					break;
+				}
+				previousNode = currentNode;
+				currentNode = currentNode.next;	// node incrementor 
+			}
+		}
+		printLinkedList(list);
+	}
+	
+	/**
+	 * Checks if afterNodeData exist in the given list
+	 * @param list
+	 * @param nodeToCheck
 	 * @return
 	 */
-	public static LinkedList insertNodeBefore(LinkedList list, int data, int beforeNodeData) {
-		Node newNode = createNewNode(data);
+	private static boolean checkIfNodeExistInList(LinkedList list, int nodeToCheck) {
+		Node currentNode = list.head;
 		
-		return list;
+		// traverse until last node in list, i.e until head pointer reaches the null value
+		while (currentNode != null) {
+			if (currentNode.data == nodeToCheck)
+				return true;
+			currentNode = currentNode.next; // increment to next node
+		}
+		return false;
 	}
+
 	
 }
